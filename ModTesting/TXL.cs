0818091a13d42710fc220eff7cc8561190a19153
eterlan <703246769@qq.com>
2022-05-20 02:46:34 +0800

    Update TXL.cs
    蒙面杀人时添加杀气

--- ModTesting/TXL.cs
+++ ModTesting/TXL.cs
@@ -13,6 +13,7 @@ namespace TXL
         public const int FaceMaskId = 888801;
         public const int BloodThirstyId = 888802;
         private System.Action<ETypeData> onKillAct;
+        private bool eventInit;
 
         public override void OnUpdate()
         {
@@ -23,73 +24,73 @@ namespace TXL
                 SuspectValue += 10;
             }
         }
-
-        void test()
-        {
-            MelonLogger.Msg("1");
-            // 测试监听
-            Action<ETypeData> testData = TestData;
-            g.events.On(EGameType.OpenUIEnd, testData);
-        }
-        // 设置他人的关系时
-        private void TestData(ETypeData e)
-        {
-            var edata = e.Cast<EGameTypeData.OpenUIEnd>();
-        }
+        
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
-            test();
-            MelonLogger.Msg("2");
-                onKillAct = OnKill;
-                //g.events.On(EGameType.WorldUnitDie, onKillAct);
-                var eType = Il2CppType.Of<UnitActionRoleKill>();
-                var e = EGameType.OneUnitCreateOneActionBack(g.world.playerUnit, eType) ;
-                
-                g.events.On(e, onKillAct);
+            Debug.Log($"sceneWasLoaded, {sceneName}");
+            if (eventInit)
+            {
+                return;
+            }
+            if (g.root == null || g.world == null)
+            {
+                Debug.Log("isNull");
+                return;
+            }
             
-            MelonLogger.Msg($"sceneWasLoaded, {sceneName}");
+            Debug.Log("Init");
+            eventInit = true;
+            onKillAct = OnKill;
+            var eType = Il2CppType.Of<UnitActionRoleKill>();
+            var e = EGameType.OneUnitCreateOneActionBack(g.world.playerUnit, eType) ;
+            
+            g.events.On(e, onKillAct);
+            
         }
 
         public override void OnApplicationQuit()
         {
             base.OnApplicationQuit();
-            //g.events.Off(EGameType.WorldUnitDie, onKillAct);
+            g.events.Off(EGameType.WorldUnitDie, onKillAct);
             Debug.Log("QUit");
-        }
-
-
-
-        public override void OnGUI() 
-        {
-            base.OnGUI();
-                // Make a background box
-                GUI.Box(new Rect(10,10,200,150), "Loader Menu");
-    
-                // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
-                if(GUI.Button(new Rect(20,40,80,20), "蒙面"))
-                {
-                    API.AddLuck(g.world.playerUnit, FaceMaskId);
-                }
-                if(GUI.Button(new Rect(120,40,80,20), "取下面罩"))
-                {
-                    API.RemoveLuck(g.world.playerUnit, BloodThirstyId);
-                }
-    
-                // Make the second button.
-                if(GUI.Button(new Rect(20,70,80,20), "开启杀气")) 
-                {
-                    API.AddLuck(g.world.playerUnit, BloodThirstyId);
-                }
-                
-                GUI.Label(new Rect(20, 100, 80, 20), "怀疑度");
-                GUI.Label(new Rect(20, 130, 80, 20), $"{SuspectValue.ToString()}");
         }
 
         private void OnKill(ETypeData eTypeData)
         {
             var eData = eTypeData.Cast<EGameTypeData.OneUnitCreateOneActionBack>();
-            var dieUnitId = eData.action.unit;
-            MelonLogger.Msg(dieUnitId);
+            var killer = eData.action.unit;
+            var hasMask = killer.GetLuck(FaceMaskId);
+            if (hasMask == null)
+            {
+                return;
+            }
+            API.AddLuck(killer, BloodThirstyId);
+        }
+        
+        public override void OnGUI() 
+        {
+            base.OnGUI();
+            // Make a background box
+            GUI.Box(new Rect(10,10,200,150), "Loader Menu");
+    
+            // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+            if(GUI.Button(new Rect(20,40,80,20), "蒙面"))
+            {
+                API.AddLuck(g.world.playerUnit, FaceMaskId);
+            }
+            if(GUI.Button(new Rect(120,40,80,20), "取下面罩"))
+            {
+                API.RemoveLuck(g.world.playerUnit, FaceMaskId);
+            }
+    
+            // Make the second button.
+            if(GUI.Button(new Rect(20,70,80,20), "开启杀气")) 
+            {
+                API.AddLuck(g.world.playerUnit, BloodThirstyId);
+            }
+                
+            GUI.Label(new Rect(20, 100, 80, 20), "怀疑度");
+            GUI.Label(new Rect(20, 130, 80, 20), $"{SuspectValue.ToString()}");
         }
     }
 }
